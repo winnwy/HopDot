df0f211f9ef28375bcd6786ecd609738a7335cc8
Winn Z <winnwyz@gmail.com>
2025-05-14 14:29:09 +1000

    Add google map to frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,56 @@
+"use client";
+import React from "react";
+
+import {
+  APIProvider,
+  Map,
+  MapCameraChangedEvent,
+} from "@vis.gl/react-google-maps";
+import PoiMarkers from "@/components/PoiMarkers";
+
+type Poi = { key: string; location: google.maps.LatLngLiteral };
+const locations: Poi[] = [
+  { key: "operaHouse", location: { lat: -33.8567844, lng: 151.213108 } },
+  { key: "tarongaZoo", location: { lat: -33.8472767, lng: 151.2188164 } },
+  { key: "manlyBeach", location: { lat: -33.8209738, lng: 151.2563253 } },
+  { key: "hyderPark", location: { lat: -33.8690081, lng: 151.2052393 } },
+  { key: "theRocks", location: { lat: -33.8587568, lng: 151.2058246 } },
+  { key: "circularQuay", location: { lat: -33.858761, lng: 151.2055688 } },
+  { key: "harbourBridge", location: { lat: -33.852228, lng: 151.2038374 } },
+  { key: "kingsCross", location: { lat: -33.8737375, lng: 151.222569 } },
+  { key: "botanicGardens", location: { lat: -33.864167, lng: 151.216387 } },
+  { key: "museumOfSydney", location: { lat: -33.8636005, lng: 151.2092542 } },
+  { key: "maritimeMuseum", location: { lat: -33.869395, lng: 151.198648 } },
+  { key: "kingStreetWharf", location: { lat: -33.8665445, lng: 151.1989808 } },
+  { key: "aquarium", location: { lat: -33.869627, lng: 151.202146 } },
+  { key: "darlingHarbour", location: { lat: -33.87488, lng: 151.1987113 } },
+  { key: "barangaroo", location: { lat: -33.8605523, lng: 151.1972205 } },
+];
+
 export default function Home() {
+  const googleMapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+  const mapId = process.env.NEXT_PUBLIC_GOOGLE_MAPS_MAP_ID;
   return (
-   <div>hello</div>
+    <APIProvider
+      apiKey={googleMapsApiKey ?? ""}
+      onLoad={() => console.log("Maps API has loaded.")}
+    >
+      <Map
+        className="w-full h-[70vh]"
+        defaultZoom={13}
+        defaultCenter={{ lat: -33.860664, lng: 151.208138 }}
+        mapId={mapId}
+        onCameraChanged={(ev: MapCameraChangedEvent) =>
+          console.log(
+            "camera changed:",
+            ev.detail.center,
+            "zoom:",
+            ev.detail.zoom
+          )
+        }
+      >
+        <PoiMarkers pois={locations} />
+      </Map>
+    </APIProvider>
   );
 }
