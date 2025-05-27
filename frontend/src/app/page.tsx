55258813f49a83c5e91caeb2b543a8c5c0cb456c
mkfeng2589 <fengmkex@hotmail.com>
2025-05-27 21:53:36 +1000

    Updated the year in the footer to update dynamically

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,17 @@
+"use client";
+
 import RunPlanner from "@/components/RunPlanner";
+import { useEffect, useState } from "react";
+
 
 export default function Home() {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => setDate(new Date()), 1000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <main className="container mx-auto px-4 py-8 flex flex-col h-screen">
       <h1 className="text-3xl font-bold mb-6">HopDot</h1>
@@ -8,7 +19,8 @@ export default function Home() {
         <RunPlanner />
       </div>
       <footer className="mt-8 text-center text-sm text-gray-500">
-        GOGOGOOSE@2025
+        GOGOGOOSE @ {date.getFullYear()}
+        {/* {date.toLocaleString()} */}
       </footer>
     </main>
   );
